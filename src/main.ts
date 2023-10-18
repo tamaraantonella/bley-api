fb6b565843b4c0d870ecbf8ef3224c0d32da294d
tamif <tamarafrazzetta@gmail.com>
2023-10-18 20:54:18 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	app.setGlobalPrefix('api/v1');
+
+	const config = new DocumentBuilder()
+		.setTitle('Bley API')
+		.setDescription('Bley API documentation')
+		.setVersion('1.0')
+		.addBearerAuth()
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('docs', app, document);
+
+	await app.listen(process.env.APP_PORT || '');
 }
 bootstrap();
